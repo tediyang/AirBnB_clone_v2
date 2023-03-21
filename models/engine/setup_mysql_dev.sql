7b984299295578b7f948b113568fe10fc88a6e01
mz-jennytech <okosisijenifer@gmail.com>
2023-03-21 14:43:38 -0700

    good

--- models/engine/setup_mysql_dev.sql
+++ models/engine/setup_mysql_dev.sql
@@ -1,7 +1,11 @@
--- prepares a MySQL server for the project
+-- This mysql script prepares a MySQL server for the project.
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Create Database if doesn't Exist and set password for user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+-- Grant all Privileges to user on hbnb_dev_db
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Grant select privileges to user on performa_schema database
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- Flush privilages
 FLUSH PRIVILEGES;
