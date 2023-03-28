acfaf787215be0e6e158609fbb4ed41f877314d3
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-03-28 20:59:23 +0100

    update city and states

--- models/city.py
+++ models/city.py
@@ -3,10 +3,13 @@
 """ importing module """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 
 class City(BaseModel, Base):
 	""" City object: Inherits from BaseModel and Base (sqlalchemy) """
 	__tablename__ = "cities"
-	name = Column(String(), nullable=False)
-	state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+	name = Column(String(128), nullable=False)
+	state_id = Column(String(60), ForeignKey('states.id'), ondelete="CASCADE", nullable=False)
+	places = relationship("Place", cascade="all", backref=backref("cities", cascade="all"),
+        passive_deletes=True)
