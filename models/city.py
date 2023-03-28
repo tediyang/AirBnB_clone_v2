147935d1c14afc10904a7c2aea3c327def865967
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-03-28 21:52:17 +0100

    update to city and db_storage

--- models/city.py
+++ models/city.py
@@ -11,5 +11,3 @@ class City(BaseModel, Base):
 	__tablename__ = "cities"
 	name = Column(String(128), nullable=False)
 	state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-	# places = relationship("Place", cascade="all", backref=backref("cities", cascade="all"),
-    #                      passive_deletes=True)
