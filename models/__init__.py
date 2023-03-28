fe75aaf61b392b9c28ebdf4b37ea4649a39e52f4
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-03-28 15:53:39 +0100

    added db storage

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,16 @@
-#!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+#!/usr/bin/python
+"""
+    Make necessary imports.
+"""
+from os import getenv
+from models.engine import *
 
+# if storage is db.
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    storage = db_storage.DBStorage()
+    storage.reload()
+else:
+    storage = file_storage.FileStorage()
+    storage.reload()
 
-storage = FileStorage()
-storage.reload()
+__all__ = ["user", "amenity", "city", "place", "review", "state"]
