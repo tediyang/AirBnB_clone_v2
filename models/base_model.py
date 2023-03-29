0a7c367133972d3497936a401b779166d684f4e1
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-03-29 09:02:16 -0700

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ BaseModel for AirBnB Clone Project. """
-""" Importing necessary modules. """
 import uuid
 from datetime import datetime
 import models
@@ -12,29 +11,25 @@ Base = declarative_base()
 
 
 class BaseModel:
-    """
-	    This is the BaseModel where all the
-	    variables and method defined and other classes
-        will inherit from.
-    """
+    """ This is the BaseModel where all the
+	variables and method defined and other classes
+        will inherit from. """
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """ Instantiates a new model """
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                 '%Y-%m-%datetime%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                 '%Y-%m-%datetime%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+	""" Initialization of the variabes """
+	if kwargs:
+            for key, value in kwargs.items():
+		if key in ['created_at', 'updated_at']:
+		    setattr(self, key, datetime.fromisoformat(value))
+		elif key != "__class__":
+		    setattr(self, key, value)
+	else:
+            self.id = str(UD.uuid4())
+	    self.created_at = datetime.now()
+	    self.updated_at = self.created_at
 
     def __str__(self):
         """ Returns a string representation of the instance """
