86ef383fa2a9b3821599d73f71109af9df4ae4ae
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-03-29 02:21:14 +0100

    amenity and place many to many

--- models/place.py
+++ models/place.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 """ Place Module for AirBnB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship, backref
 from os import getenv
 from models.review import Review
+from models.amenity import Amenity
+
+relationship_place_amen = Table("place_amenity", Base.metadata,
+                 Column('place_id', String(60), ForeignKey('places.id'),
+                 primary_key=True, nullable=False),
+                 Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                 primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -20,12 +27,14 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
-    # amenity_ids = []
+    amenity_ids = []
     places = relationship("Review",
                          backref=backref("place", cascade="all,delete"),
                          cascade="all, delete, delete-orphan",
                          passive_deletes=True,
                          single_parent=True)
+    amenities = relationship("Amenity", secondary='place_amenity',
+                         viewonly=False, backref="place")
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
@@ -36,3 +45,17 @@ class Place(BaseModel, Base):
             reviews_ = [obj for obj in storage.all(Review)
                          if obj.place_id == self.id]
             return reviews_
+
+        @property
+        def amenities(self):
+            """ Return attribute amenities_ids """
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Appends an amenity id to the attribute
+            amenity_id
+            """
+            if type(obj) == Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
+                
