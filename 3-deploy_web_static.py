3062b93407f9ea370cf0810957b2a31bb9d2f2cd
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-04-15 06:56:44 -0700

    3-deploy_web_static.py

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Fabfile to deploy to remote server """
+
+from fabfile.api import env
+from 1-pack_web_static import do_pack
+from 2-do_deploy_web_static import do_deploy
+
+env.user = "ubuntu"
+env.hosts = ["52.72.26.101", "100.25.162.157"]
+env.key_filename = '~/.ssh/id_rsa'
+
+
+def deploy():
+    """ Automatically convert to archive and deploy """
+
+    # pack the files
+    archive = do_pack()
+    if not archive:
+        return False
+
+    return do_deploy(archive)
