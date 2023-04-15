171336e5d14e152197c65034f6b39ba9d8eb5c58
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-04-15 20:20:00 +0100

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,18 +3,55 @@
     Fabfile to deploy to remote server
 """
 
-do_pack = __import__("1-pack_web_static").do_pack
-do_deploy = __import__("2-do_deploy_web_static").do_deploy
+from fabric.api import run, env, put
+from time import strftime
+import os
 
+env.user = "ubuntu"
+env.hosts = ["52.72.26.101", "100.25.162.157"]
+env.key_filename = '~/.ssh/id_rsa'
+
+
+def do_pack():
+    """ A script that generates archive the contents of web_static folder"""
+
+    filename = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
+
+        return "versions/web_static_{}.tgz".format(filename)
+
+    except Exception as e:
+        return None
+
+def do_deploy(archive_path):
+    """a function to deploy code and decompress it"""
+
+    if not os.path.isfile(archive_path):
+        return False
+    compressed_file = archive_path.split("/")[-1]
+    no_extension = compressed_file.split(".")[0]
+
+    try:
+        remote_path = "/data/web_static/releases/{}/".format(no_extension)
+        sym_link = "/data/web_static/current"
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(remote_path))
+        run("sudo tar -xvzf /tmp/{} -C {}".format(compressed_file,
+                                                  remote_path))
+        run("sudo rm /tmp/{}".format(compressed_file))
+        run("sudo mv {}/web_static/* {}".format(remote_path, remote_path))
+        run("sudo rm -rf {}/web_static".format(remote_path))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -sf {} {}".format(remote_path, sym_link))
+        return True
+    except Exception as e:
+        return False
 
 def deploy():
     """function automatically convert to archive and deploy"""
-
-    from fabfile.api import env
-
-    env.user = "ubuntu"
-    env.hosts = ["52.72.26.101", "100.25.162.157"]
-    env.key_filename = '~/.ssh/id_rsa'
 
     archive = do_pack()
     if not archive:
