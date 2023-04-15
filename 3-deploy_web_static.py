06ea8b9a7d805f5846c3658025b739db0410b5a3
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-04-15 15:13:37 +0100

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,8 +4,8 @@
 """
 
 from fabfile.api import env
-from 1-pack_web_static import do_pack
-from 2-do_deploy_web_static import do_deploy
+do_pack = __import__("1-pack_web_static").do_pack
+do_deploy = __import__("2-do_deploy_web_static").do_deploy
 
 env.user = "ubuntu"
 env.hosts = ["52.72.26.101", "100.25.162.157"]
