c2844c7d752f945456c0a5575cec7e5389d0379a
mz_jennytech <okosisijenifer@gmail.com>
2023-04-23 23:20:36 +0000

    flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
+"""a script that run a flask web application """
 from flask import Flask
-"""class Flask"""
-
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """displays text
-    Returns:
-        text
-    """
+@app.route("/", strict_slashes=False)
+def index():
+    """ a Function called with / route """
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """displays text
-    Returns:
-        text
-    """
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Function called with /hbnb route """
     return "HBNB"
 
 
 if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
