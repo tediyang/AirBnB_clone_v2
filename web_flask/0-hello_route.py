2a687706b9bbbd62ab1caf84bfffe25a610c5e2f
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-04-28 10:24:05 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" Flask Hello world """
+"""Module - script that starts a Flask web application"""
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """ print hello hbnb """
-    return "Hello HBNB!"
+    """Handles the root url"""
+    return 'Hello HBNB!'
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
