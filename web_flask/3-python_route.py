9ce1c61baaf6a61565ad815ff40efd71a026cba5
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-04-28 11:35:06 +0100

    Create 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Module - script that starts a flask app """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnh():
+    """Handles the root url"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hnbn():
+    """url to display hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """using variable to create url"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is_cool"):
+    """using variable to create url"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
