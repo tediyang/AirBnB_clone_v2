acb284eb5b4e22390664b6160cb65d3463c22152
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-04-28 10:53:46 +0100

    Create 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Module - script that starts a flask app """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnh():
+    """Handles the root url"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hnbn():
+    """url to display hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """using variable to create url"""
+    return f"C {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
