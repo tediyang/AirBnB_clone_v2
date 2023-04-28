5a45a7f787e2a8e8c35a56521f16a8c9fbbf89de
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-04-28 10:21:05 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-	""" print hello hbnb """
-	return "Hello HBNB!"
+    """ print hello hbnb """
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
