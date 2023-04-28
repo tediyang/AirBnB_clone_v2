f04299d2c808bb6dcaa35ba407fbd87e47805ae8
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-04-28 10:34:03 +0100

    Create 1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Module - script that startup a flask app """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Handles the root url"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """url to display hbnb"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
