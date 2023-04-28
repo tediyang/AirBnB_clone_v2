3e9116514776feacfeb13659f4d89dbf5a331f38
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-04-28 02:10:28 -0700

    0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Flask Hello world """
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+	""" print hello hbnb """
+	return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=5000)
