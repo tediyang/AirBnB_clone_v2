5499feefb026c2507e1a641a60db04ae1a9f807b
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-04-28 17:40:15 +0100

    added close to file storage and DBstorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -30,6 +30,8 @@ class FileStorage:
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
+            """ Update the dictionary by passing in key:value pairs dict
+            of the previously loaded data. """
             temp.update(FileStorage.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
@@ -64,7 +66,18 @@ class FileStorage:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
+                """ Since the values are saved in FileStorage.__objects as:
+                    e.g Place.123456678 : Place 
+                    # meaning --- The Place (object has all its attributes/ values)
+                    Then we have to load the data into FileStorage by passing
+                    {Place.123456678 : Place(**val)} this will call the Place object
+                    with it value loaded.
+                """
                 for key, val in temp.items():
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+        
+    def close(self):
+        """Close the session."""
+        self.reload()
