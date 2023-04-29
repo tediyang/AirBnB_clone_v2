c5ea09dcda10118f2ded5ae60f12b76daee3a12f
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-04-29 17:10:02 +0100

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """Your web application must be listening on 0.0.0.0, port 5000
 You must use storage for fetching data from the storage engine
 (FileStorage or DBStorage) => from models import storage and storage.all(...)
