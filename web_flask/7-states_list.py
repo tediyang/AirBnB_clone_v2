f2f6b165acbc4bd9b5a1ef04036a207238ce8097
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-04-29 15:44:07 +0100

    State List"

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-""" Display the states data stored in the database """
+""" Display the States data stored in the database """
 from flask import Flask
 from flask import render_template
-from 
+from models import *
+from models.__init__ import storage
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
 def teardown():
+    """ Close the session """
     return storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def state_list():
+    """ state list function """
+    states = storage.all(state.State)
+    return render_template("7-states_list.html", state=state)
     
     
 
