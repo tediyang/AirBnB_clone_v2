743870f38a3630010af512c253e09c0eb5d9b793
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-05-01 09:55:44 -0700

    Create 8-cities_by_states.py

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+    Your web application must be listening on 0.0.0.0, port 5000
+    You must use storage for fetching data from the storage engine
+    (FileStorage or DBStorage) => from models import storage and storage.all(...)
+    After each request you must remove the current SQLAlchemy Session:
+    Declare a method to handle @app.teardown_appcontext
+    Call in this method storage.close()
+    Routes:
+    /states_list: display a HTML page: (inside the tag BODY)
+    H1 tag: “States”
+    UL tag: with the list of all State objects present in DBStorage sorted by
+    name (A->Z) tip
+    LI tag: description of one State: <state.id>: <B><state.name></B>
+"""
+from flask import Flask
+from flask import render_template
+from models.state import State
+from models import storage
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown():
+    """ Close the session """
+    return storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """ state list function """
+    states = storage.all(State)
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
