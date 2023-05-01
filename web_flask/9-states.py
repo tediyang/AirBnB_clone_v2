0f2ab1e396100e2597a416cbe270cc957fd24070
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-05-01 11:30:46 -0700

    Create 9-states.py

---
+++ web_flask/9-states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+    Your web application must be listening on 0.0.0.0, port 5000
+    You must use storage for fetching data from the storage engine
+    (FileStorage or DBStorage) => from models import storage and storage.all(...)
+    After each request you must remove the current SQLAlchemy Session:
+    Declare a method to handle @app.teardown_appcontext
+    Call in this method storage.close()
+    Routes:
+    /9-states: display a HTML page: (inside the tag BODY)
+"""
+from flask import Flask
+from flask import render_template
+from models.state import State
+from models import storage
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown():
+    """ Close the session """
+    return storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def state_list():
+    """ state list function """
+    states = storage.all(State)
+    return render_template("9-states.html", states=states, id=None)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_list(id):
+    """ Get state and cities with the provided id. """
+    states = storage.all(State)
+    for state_obj in all_states.values():
+        if state_obj.id == id:
+            state = state_obj
+            return render_template("9-states.html", states=state, id=id)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
